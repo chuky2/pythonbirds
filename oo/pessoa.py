42b50debbfd97a5843319a1a4a492174187bc8c5
Pedro Neves <pedro.braghini@gmail.com>
2020-10-13 23:23:10 -0300

    Coisa bagarai

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,3 +1,36 @@
 class Pessoa:
-    pass
+    olhos = 2
 
+    def __init__(self, *filhos, nome=None, idade=35):
+        self.idade = idade
+        self.nome = nome
+        self.filhos = list(filhos)
+
+    def cumprimentar(self):
+        return 'Olá'
+
+    @staticmethod
+    def metodo_estatico():
+        return 42
+
+    @classmethod
+    def nome_e_atributos_de_classe(cls):
+        return f'{cls} - olhos {cls.olhos}'
+
+if __name__ == '__main__':
+    renzo = Pessoa(nome='Renzo')
+    luciano = Pessoa(renzo, nome='Luciano')
+    print(Pessoa.cumprimentar(luciano))
+    print(luciano.cumprimentar())
+    print(luciano.nome)
+    print(luciano.idade)
+    for filho in luciano.filhos:
+        print(filho.nome)
+    luciano.sobrenome='Ramalho'
+    print(luciano.sobrenome)
+    print(luciano.__dict__)
+    print(Pessoa.olhos)
+    print(luciano.olhos)
+    print(renzo.olhos)
+    print(Pessoa.metodo_estatico())
+    print(Pessoa.nome_e_atributos_de_classe())
